22ad444aa9928bff74389de4d6bbd5d991d8ef79
PPY2204 <nguyenvandu222k4@gmail.com>
2025-07-02 02:27:04 +0700

    #55 feat: create homepage screen for FE

---
+++ fe/src/pages/client/home/components/HelpSection.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Button } from '@/components/ui/button';
+import { HELP_ICONS, HELP_TEXTS } from '../data';
+
+
+const HelpSection: React.FC = () => {
+    const navigate = useNavigate();
+    return (
+        <section className=" mx-auto my-8 text-center">
+            <h2 className="text-4xl font-bold my-16">How You Can Help?</h2>
+
+            <div className="flex flex-wrap justify-center gap-16 cursor-pointer">
+                {HELP_TEXTS.map((text, index) => (
+                    <div key={index} className="flex flex-col items-center gap-12 max-w-xs">
+                        <img src={HELP_ICONS[index]} alt={`Icon ${index + 1}`} className="w-20 h-20" />
+                        <p className="text-gray-700 text-xl">{text}</p>
+                    </div>
+                ))}
+            </div>
+
+            <Button
+                variant="ghost"
+                className="my-16 bg-[#005DB9] hover:bg-blue-700 text-white hover:text-white text-2xl font-semibold px-8 py-4 h-16 w-3xs rounded"
+                onClick={() => navigate('/report')}
+            >
+                File A Report
+            </Button>
+
+        </section>
+    );
+};
+
+export default HelpSection;
--- fe/src/routes/AppRoutes.tsx
+++ fe/src/routes/AppRoutes.tsx
@@ -1,21 +1,26 @@
-import LoginPage from '@/pages/auth/LoginPage'
-import RegisterPage from '@/pages/auth/RegisterPage'
 import { createBrowserRouter } from 'react-router-dom'
+import HomePage from '@/pages/client/home/HomePage'
+import LoginPage from '@/pages/admin/auth/LoginPage'
+import RegisterPage from '@/pages/admin/auth/RegisterPage'
 
-const AppRoutes = createBrowserRouter([{
-    path: '/',
-    children: [
-        {
-            path: '/login',
-            element: <LoginPage />,
-        },
-        {
-            path: '/register',
-            element: <RegisterPage />,
-        }
-
-    ]
-}])
-
+const AppRoutes = createBrowserRouter([
+    {
+        path: '/',
+        element: <HomePage />,
+    },
+    {
+        path: '/admin',
+        children: [
+            {
+                path: 'login',
+                element: <LoginPage />,
+            },
+            {
+                path: 'register',
+                element: <RegisterPage />,
+            },
+        ],
+    },
+])
 
 export default AppRoutes
